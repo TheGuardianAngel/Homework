531a8a10866e32f37a457e13fa044474e8efa82e
Angel Georgiev <angel14102002@gmail.com>
2017-10-10 11:50:21 +0300

    Started learning methods! Hype!!!

---
+++ Methods/Methods_Ex/Ex.2.1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex._2._1
+{
+    class Program
+    {
+        static void PrintSign(int num)
+        {
+            if(num > 0)
+                Console.WriteLine($"The number {num} is positive.");
+            else if(num < 0)
+                Console.WriteLine($"The number {num} is negative.");
+            else
+                Console.WriteLine($"The number {num} is zero.");
+
+        }
+
+        static void Main(string[] args)
+        {
+            PrintSign(int.Parse(Console.ReadLine()));
+        }
+    }
+}
---
+++ Methods/Methods_Ex/Methods_Ex/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_Ex
+{
+    class Program
+    {
+
+        static void ReceiptHeader()
+        {
+            Console.WriteLine("CASH RECEIPT");
+            Console.WriteLine(new string('-', 30));
+        }
+        static void ReceiptBody(string charged, string recieved)
+        {
+            Console.WriteLine("Charged to " + charged);
+            Console.WriteLine("Recieved by " + recieved);
+        }
+        static void ReceiptFooter()
+        {
+            Console.WriteLine(new string('-', 30));
+            Console.WriteLine("\u00A9 SoftUni");
+        }
+      
+
+        static void Main(string[] args)
+        {
+            ReceiptHeader();
+            ReceiptBody("Angel", "IDK" );
+            ReceiptFooter();
+        }
+
+       
+    }
+}
