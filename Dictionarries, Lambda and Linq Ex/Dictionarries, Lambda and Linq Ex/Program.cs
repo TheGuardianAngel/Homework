19b0dbd19624ce6630bc013331e4ac74414484ad
Angel Georgiev <angel14102002@gmail.com>
2017-10-21 16:56:27 +0300

    Started learning dictionaries!

---
+++ Dictionarries, Lambda and Linq Ex/Dictionarries, Lambda and Linq Ex/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionarries__Lambda_and_Linq_Ex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var phonebook = new Dictionary<string, string>();
+            phonebook.Add("Sofia", String.Empty);
+            phonebook["Nakov"] = "0889227840";
+            phonebook["Pesho"] = "0763 / 76423";
+            phonebook["Nakov"] = "new phone -> 0987654321";
+            //phonebook.Remove("Nakov");
+            phonebook["Ani"] = "1234567890";
+
+            if (phonebook.ContainsKey("Sofia"))
+            {
+                phonebook.Remove("Sofia");
+            }
+
+            foreach (var item in phonebook)
+            {
+                Console.WriteLine("{0} -> {1}", item.Key, item.Value);
+            }
+        }
+    }
+}
