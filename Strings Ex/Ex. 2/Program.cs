f36b3d926061065d866869150e0064577dc4023a
Angel Georgiev <angel14102002@gmail.com>
2017-11-03 15:48:12 +0200

    Strings an StringBuilder

---
+++ Strings Ex/Ex. 2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var str = new StringBuilder();
+            for (int i = 0; i < 1000000; i++)
+            {
+                str.Append("PESHO");
+            }
+            Console.WriteLine(str.ToString());
+        }
+    }
+}
---
+++ Strings Ex/Strings Ex/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings_Ex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var forbiddenWords = Console.ReadLine().Split(',', ' ').Where(w => w.Length > 0).ToArray();
+            var words = Console.ReadLine();
+
+            foreach (var banned in forbiddenWords)
+            {
+                if (words.Contains(banned))
+                {
+                    words = words.Replace(banned, new string('*', banned.Length));
+                }
+            }
+            Console.WriteLine(words);
+
+            
+        }
+    }
+}
